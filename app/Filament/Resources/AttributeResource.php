a8260fc43cf1ee5ea29a3b606687ad6f711ef5cd
grzegorz <grzegorzlis90@gmail.com>
2025-04-21 16:54:50 +0200

    dodano attribute resource

---
+++ app/Filament/Resources/AttributeResource.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AttributeResource\Pages;
+use App\Filament\Resources\AttributeResource\RelationManagers;
+use App\Models\Attribute;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Enums\AttributeType;
+
+
+class AttributeResource extends Resource
+{
+    protected static ?string $model = Attribute::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('display_name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Select::make('type')
+                    ->options(AttributeType::class) 
+                    ->enum(AttributeType::class)
+                    ->required()
+                    ->native(false) 
+                    ->searchable(),
+                Forms\Components\TextInput::make('short_name')
+                    ->maxLength(255)
+                    ->default(null),
+                Forms\Components\TextInput::make('unit')
+                    ->maxLength(255)
+                    ->default(null),
+                Forms\Components\TextInput::make('order')
+                    ->required()
+                    ->numeric()
+                    ->default(0),
+                Forms\Components\TextInput::make('min')
+                    ->numeric()
+                    ->default(null),
+                Forms\Components\TextInput::make('max')
+                    ->numeric()
+                    ->default(null),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('display_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('type'),
+                Tables\Columns\TextColumn::make('short_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('unit')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('order')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('min')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('max')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAttributes::route('/'),
+            'create' => Pages\CreateAttribute::route('/create'),
+            'edit' => Pages\EditAttribute::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/Attribute.php
+++ app/Models/Attribute.php
@@ -1,10 +1,17 @@
 <?php
 
 namespace App\Models;
+use App\Enums\AttributeType;
+
 
 use Illuminate\Database\Eloquent\Model;
 
 class Attribute extends Model
 {
-    //
+
+    protected $fillable = ['name', 'type','display_name','short_name','unit','order','min','max'];
+
+    protected $casts = [
+        'type' => AttributeType::class,
+    ];
 }
