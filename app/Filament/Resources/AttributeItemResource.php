8d2e76d8089c784b7535163d6c5a9b82235825df
grzegorz <grzegorzlis90@gmail.com>
2025-04-22 19:00:16 +0200

    utworzono model, migrację oraz filament resource dla tabeli attribute_items

---
+++ app/Filament/Resources/AttributeItemResource.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AttributeItemResource\Pages;
+use App\Filament\Resources\AttributeItemResource\RelationManagers;
+use App\Models\AttributeItem;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AttributeItemResource extends Resource
+{
+    protected static ?string $model = AttributeItem::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('attribute_id')
+                    ->numeric()
+                    ->default(null),
+                Forms\Components\TextInput::make('value')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\FileUpload::make('image')
+                    ->image()
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('attribute_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('value')
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('image'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAttributeItems::route('/'),
+            'create' => Pages\CreateAttributeItem::route('/create'),
+            'edit' => Pages\EditAttributeItem::route('/{record}/edit'),
+        ];
+    }
+}
